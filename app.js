2fd9cb4339393a3d2d45d506e2aaef09278f638c
Kyle Turco <kyleturco@gmail.com>
2015-08-03 10:41:38 -0500

    add error handling

--- app.js
+++ app.js
@@ -11,10 +11,23 @@ app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
 
-app.use(function (req, res) {
-  res.status(403);
-  res.send('Unauthorized!');
+app.get('/json', function (req, res) {
+  res.send({an: 'object'});
+});
+
+app.get('/thisshoulderror', function (req, res) {
+  res.send(badVariable);
+});
+
+app.use(function (err, req, res, next) {
+  // pass 4 arguments to create an error handling middleware
+  console.log('ERRRoRoRR!', err.stack);
+  res.status(500).send('My B, brah');
 })
+
+app.use(function (req, res) {
+  res.status(403).send('Unauthorized!');
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
