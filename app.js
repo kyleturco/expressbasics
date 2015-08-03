3ae738a381d59ee4d415600759812aa2ac2947ce
Kyle Turco <kyleturco@gmail.com>
2015-08-03 14:39:23 -0500

    update 'app' to 'router'

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 
-app.set('view engine', 'ejs');
+router.set('view engine', 'ejs');
 
-app.use(function (req, res, next) {
+router.use(function (req, res, next) {
   // logging at the top
   console.log('Request at ' + new Date().toISOString());
   next();
 });
-
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
+router.use(express.static('public'));
+router.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-app.get('/pizza/:topping/:qty', function (req, res) {
+router.get('/pizza/:topping/:qty', function (req, res) {
   var obj = req.params;
   obj.title = 'Pizza Shop';
 
   res.render('templates/pizza', obj);
 });
-
-app.get(/hello/, function (req, res) {
+router.get(/hello/, function (req, res) {
   res.send('Hello!');
 });
-
-app.get('/awesomethings', function (req, res) {
+router.get('/awesomethings', function (req, res) {
   setTimeout(function () {
     var awesomeThings = [
       'Pizza',
@@ -44,35 +40,29 @@ app.get('/awesomethings', function (req, res) {
     );
   }, 5000);
 });
-
-app.get('/test', function (req, res, next) {
+router.get('/test', function (req, res, next) {
   res.write('Test1!');
   next();
 });
-
-app.get('/test', function (req, res) {
+router.get('/test', function (req, res) {
   res.end('Test2!');
 });
-
-app.get('/json', function (req, res) {
+router.get('/json', function (req, res) {
   res.send({an: 'object'});
 });
-
-app.get('/thisshoulderror', function (req, res) {
+router.get('/thisshoulderror', function (req, res) {
   res.send(badVariable);
 });
-
-app.use(function (req, res) {
+router.use(function (req, res) {
   res.status(403).send('Unauthorized!');
 });
-
-app.use(function (err, req, res, next) {
+router.use(function (err, req, res, next) {
   // pass 4 arguments to create an error handling middleware
   console.log('ERRRRRRRRRR', err.stack);
   res.status(500).send('My Bad');
 });
 
-var server = app.listen(3000, function () {
+var server =router.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
