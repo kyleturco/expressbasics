d4cc40dc20078053e68f08521f41ac82a5fc4451
Kyle Turco <kyleturco@gmail.com>
2015-08-04 10:56:15 -0500

    add loggly

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
-var express = require('express');
-var app = express();
-// var app = require express execute
-var routes = require('./routes/index');
-var pizza = require('./routes/pizza');
-var lessCSS = require('less-middleware');
-var morgan = require('morgan');
 var fs = require('fs');
 
+var express = require('express');
+var lessCSS = require('less-middleware');
+var morgan = require('morgan');
+
+var routes = require('./routes/index');
+var pizza = require('./routes/pizza');
+
+var app = express();
 
 app.set('view engine', 'ejs');
 app.set('case sensitive routing', true);
 
-app.locals.title = 'AwesomeTown';
+app.locals.title = 'aweso.me';
 
 app.use(lessCSS('public'));
 
-app.use(function(req, res, next){
-  console.log('Request at ' + new Date().toISOString());
-  next();
-})
-//handles a console log for requests at any url
-
 var logStream = fs.createWriteStream('access.log', {flags: 'a'});
-app.use(morgan('dev', {stream: logStream}));
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
 
-app.use(express.static('public'))
-//called a middleware
-//essentially creates a route for everything in the public folder
+app.use(function (req, res, next) {
+  var client = require('./lib/loggly')('incoming');
+
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method
+  });
+  next();
+});
+
+app.use(express.static('public'));
 
 app.use('/', routes);
 app.use('/pizza', pizza);
 
-
-app.use(function(req, res, next){
+app.use(function (req, res) {
   res.status(403).send('Unauthorized!');
 });
-//this handles routes that dont exits
-//so if you try to go to a route that doesnt exist is changes the response status code to 403 and sends "Unauthorized" as the res text
-//it has to be at the bottom of the page otherwise it over-writes all routes with this response
-//put 400 error handling before 500 error handling
 
-app.use(function(err, req, res, next){
-  //pass 4 arguments to create an error handling middleware
-  console.log("error", err.stack);
-  res.status(500).send('500: Error!')
-})
+app.use(function (err, req, res, next) {
+  var client = require('./lib/loggly')('error');
 
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method,
+    stackTrace: err.stack
+  });
+
+  // pass 4 arguments to create an error handling middleware
+  console.log('ERRRRRRRRRR', err.stack);
+  res.status(500).send('My Bad');
+});
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%d', host, port);
 });
