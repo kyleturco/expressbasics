f6bc5024581c403e463a8ae6572a8f4aabb82a16
Kyle Turco <kyleturco@gmail.com>
2015-08-05 11:24:09 -0500

    start file upload functionality, add rule to only accept images

--- routes/imgur.js
+++ routes/imgur.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var upload = require('multer')({dest: 'uploads/'})
+var upload = require('multer')({
+  dest: 'uploads/',
+  fileFilter: function (req, file, cb) {
+    cb(null, file.mimetype.slice(0, 6) === 'image/');
+  }
+});
 
 router.get('/', function(req, res){
   res.render('templates/imgur');
 })
 
-router.post('/upload', function(req, res){
+router.post('/upload', upload.single('file'), function(req, res){
+  console.log(req.file);
   res.redirect('/imgur');
 });
 
