9451380cca3a7844723bfa3ef8c0156ce509ee7f
Kyle Turco <kyleturco@gmail.com>
2015-08-07 11:54:27 -0500

    update chicken index and mongo orders list

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,19 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var ObjectID = require('mongodb').ObjectID;
+
 
 router.get('/', function (req, res) {
-  res.render('templates/chicken-index');
+  var collection = global.db.collection('chickenNuggets')
+
+  collection.find({}).toArray(function(err, orders) {
+    var formattedOrders = orders.map(function (order) {
+      return {
+        _id:        order._id,
+        name:       order.name,
+        flavor:     order.style,
+        quantity:   order.qty,
+        createdAt:  order._id.getTimestamp(),
+      }
+    });
+
+    res.render('templates/chicken-index', {orders: formattedOrders});
+
+  });
 });
 
 router.get('/', function (req, res) {
   res.render('templates/chicken-new');
 });
 
-router.post('/order', function (req, res) {
+router.post('/order/:id/complete', function (req, res) {
   var collection = global.db.collection('chickenNuggets');
 
-  collection.save(req.body, function () {
-    res.redirect('/')
+  collection.update(
+    {_id: ObjectID(req.params.id)},
+    {$set: {complete: true}},
+    function () {
+      res.redirect('/chickennuggets')
   });
 });
 
